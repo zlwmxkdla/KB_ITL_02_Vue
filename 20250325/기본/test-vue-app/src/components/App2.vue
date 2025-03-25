c7a3ef46363d5805266ae7e8e772e66ad304b752
zlwmxkdla@naver.com <zlwmxkdla@naver.com>
2025-03-25 17:42:37 +0900

    20250325심화

---
+++ 20250325/기본/test-vue-app/src/components/App2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>관심있는 K-POP 가수?</h2>
+    <hr />
+    <ul>
+      <CheckboxItem2 v-for="idol in idols" :key="idol.id" :idol="idol" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem2 from './components/CheckboxItem2.vue';
+
+export default {
+  name: 'App',
+  components: { CheckboxItem2 },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'BTS', checked: true },
+        { id: 2, name: 'Black Pink', checked: false },
+        { id: 3, name: 'EXO', checked: false },
+        { id: 4, name: 'ITZY', checked: false },
+      ],
+    };
+  },
+};
+</script>
---
+++ 20250325/기본/test-vue-app/src/components/CheckboxItem2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <li><input type="checkbox" :checked="idol.checked" />{{ idol.name }}</li>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CheckedboxItem2',
+  props: ['idol'],
+};
+</script>
+<style></style>
