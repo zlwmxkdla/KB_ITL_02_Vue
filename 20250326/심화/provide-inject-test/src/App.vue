0384062d031ed0914772b7898ea05b3a5f9c91a1
zlwmxkdla@naver.com <zlwmxkdla@naver.com>
2025-03-26 16:20:33 +0900

    20250326심화

---
+++ 20250326/심화/provide-inject-test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>최신 인기곡</h2>
+  </div>
+</template>
+<script>
+import { computed } from 'vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+      songs: [
+        { id: 1, title: 'Blueming', done: true },
+        { id: 2, title: 'Dynamite', done: true },
+        { id: 3, title: 'Lovesick Girls', done: false },
+        { id: 4, title: '마리아 (Maria)', done: false },
+      ],
+    };
+  },
+  provide() {
+    return {
+      icons: {
+        checked: 'far facheck circle',
+        unchecked: 'far facircle',
+      },
+      doneCount: computed(() => {
+        return this.songs.filter((s) => s.done === true).length;
+      }),
+    };
+  },
+};
+</script>
+<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/fontawesome/5.14.0/css/all.min');
+</style>
