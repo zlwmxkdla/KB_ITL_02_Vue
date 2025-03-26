6afeef133965080f2d3155c461d14095813c8b07
zlwmxkdla@naver.com <zlwmxkdla@naver.com>
2025-03-26 15:42:38 +0900

    20250326기본

---
+++ 20250326/기본/slot-test/src/components/SlotTest.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3>당신이 경험한 프론트 엔드 기술은?(첫 번째:Slot사용(X))</h3>
+    <CheckBox2
+      v-for="item in items"
+      :key="item.id"
+      :id="item.id"
+      :label="item.label"
+      :checked="item.checked"
+      @check-changed="CheckBoxChanged"
+    >
+      <span v-if="item.checked" style="color: blue; text-decoration: underline">
+        <i>{{ item.label }}</i>
+      </span>
+    </CheckBox2>
+  </div>
+</template>
+<script>
+import CheckBox2 from './CheckBox2.vue';
+
+export default {
+  name: 'SlotTest',
+  components: { CheckBox2 },
+  data() {
+    return {
+      items: [
+        { id: 'V', checked: true, label: 'Vue' },
+        { id: 'A', checked: false, label: 'Angular' },
+        { id: 'R', checked: false, label: 'React' },
+        { id: 'S', checked: false, label: 'Svelte' },
+      ],
+    };
+  },
+  methods: {
+    CheckBoxChanged(e) {
+      let item = this.items.find((item) => item.id === e.id);
+      item.checked = e.checked;
+    },
+  },
+};
+</script>
+<style></style>
