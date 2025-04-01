aa63f1ffa148c772d29da85ad6b77ce9a8750878
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-04-02 00:16:56 +0900

    Update App.vue

--- 20250328/심화/fake-store/src/App.vue
+++ 20250328/심화/fake-store/src/App.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import NavBar from './components/NavBar.vue'
+
 </script>
 
 <template>
   <header>
     <Header />
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <NavBar />
+   
   </header>
-  
   <RouterView />
+    <Footer />
+  
 </template>
